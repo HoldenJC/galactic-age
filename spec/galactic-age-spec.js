cd781e9f15ded95c9a6ffebfee06acb66e1483ed
HoldenJC <holdenj_c@yahoo.com>
2019-06-28 12:43:03 -0700

    add working tests for mercury, venus, mars, and jupiter age conversions

--- spec/galactic-age-spec.js
+++ spec/galactic-age-spec.js
@@ -11,26 +11,26 @@ describe('Human', function() {
 
   it('should test that a Human can be made and assigned an ageEarth', function() {
 
+    testHuman.extraterrestrialAge();
+
     expect(testHuman.name).toEqual("Billy");
     console.log(testHuman.name + " compare " + "Billy");
 
     expect(testHuman.ageEarth).toEqual(24);
     console.log(testHuman.ageEarth + " compare " + 24);
 
-    // expect(testRole.intelligence).toEqual(1);
-    console.log(testHuman.ageMercury + " compare " + 0);
+    expect(testHuman.ageMercury).toEqual(5.76);
+    console.log(testHuman.ageMercury + " compare " + 5.76);
 
-    // expect(testRole.intelligence).toEqual(1);
-    console.log(testHuman.ageVenus + " compare " + 0);
+    expect(testHuman.ageVenus).toEqual(14.88);
+    console.log(testHuman.ageVenus + " compare " + 14.88);
 
-    // expect(testRole.intelligence).toEqual(1);
-    console.log(testHuman.ageMars + " compare " + 0);
+    expect(testHuman.ageMars).toEqual(45.12);
+    console.log(testHuman.ageMars + " compare " + 45.12);
 
-    // expect(testRole.intelligence).toEqual(1);
-    console.log(testHuman.ageJupiter + " compare " + 0);
-
+    expect(testHuman.ageJupiter).toEqual(284.64);
+    console.log(testHuman.ageJupiter + " compare " + 284.64);
 
   });
-
 
 });
--- src/galactic-age.js
+++ src/galactic-age.js
@@ -5,7 +5,15 @@ export class Human {
     this.ageMercury = 0;
     this.ageVenus = 0;
     this.ageMars = 0;
-    this.ageJupiter = 0; 
-
+    this.ageJupiter = 0;
   }
+
+  extraterrestrialAge(){
+    this.ageMercury = ((this.ageEarth * 0.24).toFixed(2)) * 1;
+    this.ageVenus = ((this.ageEarth * 0.62).toFixed(2)) * 1;
+    this.ageMars = ((this.ageEarth * 1.88).toFixed(2)) * 1;
+    this.ageJupiter = ((this.ageEarth * 11.86)).toFixed(2)) * 1;
+  }
+
+
 }
